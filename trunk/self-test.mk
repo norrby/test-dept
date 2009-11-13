598b70fbf605d514c947219e6188906ed41c4997
m.norrby <m.norrby@ce6fa33e-8ec7-11dd-8d41-03ea52202348>
2009-11-13 06:37:27 +0000

    compilation paths

--- trunk/self-test.mk
+++ trunk/self-test.mk
@@ -8,7 +8,7 @@ EXAMPLE_PROJECTS=automatic_project example_project off_dir simple_project
 EXAMPLES=$(addprefix examples/,$(EXAMPLE_PROJECTS))
 
 $(EXAMPLES):
-	$(BINPATH_BASE)/../src $(MAKE) -C $@ -I../../src test_dept
+	$(BINPATH_BASE)/../src $(MAKE) -C $@ -I../../src CPPFLAGS=-I../../src test_dept
 
 feature-test: $(EXAMPLES)
 	src/test_dept $(addsuffix /*_test,$^)
