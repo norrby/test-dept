0757625e772c77b9dc39960f6c87821d701d5482
m.norrby <m.norrby@ce6fa33e-8ec7-11dd-8d41-03ea52202348>
2010-10-07 14:16:27 +0000

    remove debug echo

--- trunk/src/test-dept.mk
+++ trunk/src/test-dept.mk
@@ -41,7 +41,7 @@ TEST_MAINS=$(patsubst %_main.c,%,$(TEST_MAIN_SRCS))
 # Constructed in order to see which symbols are resolved by the loader.
 # Such symbols that we do not want to stub could be stdout, stderr, etc
 %_tmpmain.o:	%.o
-	$(CC) $(LDFLAGS) $(LDFLAGS_UNRESOLVED) $(TARGET_ARCH)	$^ -o $@ && nm $@
+	$(CC) $(LDFLAGS) $(LDFLAGS_UNRESOLVED) $(TARGET_ARCH)	$^ -o $@
 
 %_proxies.s:	%.o %_test_main.o
 	$(TEST_DEPT_RUNTIME_PREFIX)sym2asm $^ $(SYMBOLS_TO_ASM) $(NM) >$@
